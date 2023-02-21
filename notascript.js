1050afe655b793585676d6c382aaa93deae34547
Alejandro Tobon <alejo.tobon01@gmail.com>
2023-02-21 14:55:35 -0500

    Mostrar una alerta de la nota al guardar

---
+++ notascript.js
@@ -0,0 +1,13 @@
+ const formulario = document.querySelector("#agregar-nota") ;
+
+ function even(evento) {
+    alert(
+        evento.target.titulo.value + ":" +" " +
+        evento.target.descripcion.value
+        );
+
+ };  
+
+ formulario.addEventListener("submit", even);
+
+ 
