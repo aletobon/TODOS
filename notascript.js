4b19390665ad63818b17bd44ec9e510a21120efd
Alejandro Tobon <alejo.tobon01@gmail.com>
2023-02-22 08:17:40 -0500

    borrar espacios

--- notascript.js
+++ notascript.js
@@ -1,13 +1,8 @@
  const formulario = document.querySelector("#agregar-nota") ;
-
  function even(evento) {
     alert(
         evento.target.titulo.value + ":" +" " +
         evento.target.descripcion.value
         );
-
  };  
-
  formulario.addEventListener("submit", even);
-
- 
