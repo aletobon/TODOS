bab555f59a319cdc5c077807428c077e76358d4c
Alejandro Tobon <alejo.tobon01@gmail.com>
2023-02-22 17:35:18 -0500

    usar localStorage para leer y guardar notas

--- js/logica.js
+++ js/logica.js
@@ -10,9 +10,26 @@ Descripción: ${this.descripcion}`;
     }
 }
 
-let notas = [];
+const claveListaNotas = "notas";
+function guardarListaNotas(notas) {
+    const valorNotas = JSON.stringify(notas);
+    localStorage.setItem(claveListaNotas, valorNotas);
+}
+
+function cargarListaNotas() {
+    const valorNotas = localStorage.getItem(claveListaNotas);
+    let notas = [];
+    if (valorNotas) {
+        notas = JSON.parse(valorNotas).map(nota => new Nota(nota.titulo, nota.descripcion));
+    }
+    return notas;
+}
+
+let notas = cargarListaNotas();
 
 function guardarNota(nota) {
     notas.push(nota);
+    guardarListaNotas(notas);
     console.log(notas);
 }
+
