2a9b85096841716817885c76beaa429214b34523
Alejandro Tobon <alejo.tobon01@gmail.com>
2023-02-22 10:15:31 -0500

    usar evento en boton y no en formulario

--- notascript.js
+++ notascript.js
@@ -1,8 +1,10 @@
- const formulario = document.querySelector("#agregar-nota") ;
- function even(evento) {
-    alert(
-        evento.target.titulo.value + ":" +" " +
-        evento.target.descripcion.value
-        );
- };  
- formulario.addEventListener("submit", even);
+const formulario = document.querySelector("#agregar-nota");
+const btnAgregarNota = document.querySelector("#btn-agregar-nota");
+
+function btnClick(e) {
+    console.log(e);
+    alert(`${formulario.titulo.value}: ${formulario.descripcion.value}`);
+    e.preventDefault();
+}
+
+btnAgregarNota.addEventListener("click", btnClick);
