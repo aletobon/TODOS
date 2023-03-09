c2cd91ebf84fd8715ef8b32e541a7c5dc0e2ce2d
Alejandro Tobon <alejo.tobon01@gmail.com>
2023-03-09 10:37:34 -0500

    se agrega funtion obtener HTML

--- js/nota.js
+++ js/nota.js
@@ -8,4 +8,15 @@ class Nota {
         return `Titulo: ${this.titulo}
 Descripción: ${this.descripcion}`;
     }
+
+    obtenerHTML() {
+        return `
+        <article class="nota">
+            <input type="checkbox" />
+            <h1>${this.titulo}</h1>
+            <p> ${this.descripcion} </p>
+        </article>
+        `;
+    }
+
 }
